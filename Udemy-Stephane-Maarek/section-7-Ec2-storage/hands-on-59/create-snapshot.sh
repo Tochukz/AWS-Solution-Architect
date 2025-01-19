430e32c6a3a6148e6ff1e38a9790a792cdabdc68
Tochukwu Nwachukwu <truetochukz@gmail.com>
2025-01-19 16:58:44 +0200

    Moved files

--- Udemy-Stephane-Maarek/section-7-Ec2-storage/hands-on-59/create-snapshot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Description:  Create a snapshot from the an EBS volume 
-
-volumnId="vol-07590611e2dc87c13"
-SnapshotId=$(aws ec2 create-snapshot --volume-id $volumnId --description "Simple volume snapshot" --query 'SnapshotId')
-
-echo "SnapshotId: $SnapshotId" 
