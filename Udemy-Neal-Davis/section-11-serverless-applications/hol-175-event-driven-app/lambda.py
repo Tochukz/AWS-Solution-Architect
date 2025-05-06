43d4f9ce0dee6879844ebcbf898afc27a6ccb2a4
Tochukwu Nwachukwu <truetochukz@gmail.com>
2025-05-06 18:56:35 +0200

    Event Driven Application

---
+++ Udemy-Neal-Davis/section-11-serverless-applications/hol-175-event-driven-app/lambda.py
@@ -0,0 +1,42 @@
+import os
+import json
+import boto3
+from uuid import uuid4
+
+region_name = os.environ.get('AWS_REGION', 'eu-west-1')  # Default to u-west-1 if not set
+boto3.setup_default_session(region_name=region_name)
+dynamodb = boto3.resource('dynamodb', region_name=region_name, api_version='2012-08-10')
+doc_client = dynamodb.Table('ProductVisits')
+
+def handler(event, context):  
+    print('Received event:', json.dumps(event, indent=2))
+    
+    # Process each record in the event
+    for record in event['Records']:
+        body = record['body']
+        print(body)
+        
+        body = json.loads(body)
+        
+        try:
+            required_fields = ['ProductId', 'ProductName', 'Category', 'PricePerUnit', 'CustomerId', 'CustomerName', 'TimeOfVisit']
+            if not all(field in body for field in required_fields):
+                print('Please provide values for product, category, customer, and time of visit.')
+                continue
+            
+            body['ProductVisitKey'] = str(uuid4())
+            
+            print(f"{body['ProductVisitKey']} {body['ProductId']} {body['ProductName']} {body['Category']} {body['PricePerUnit']} {body['CustomerId']} {body['CustomerName']} {body['TimeOfVisit']}")
+            
+            params = {
+                'Item': body
+            }
+            
+            doc_client.put_item(**params)
+            
+            print('Product Visit record is successfully created.')
+        
+        except Exception as e:
+            print(e)
+            
+    return {}
