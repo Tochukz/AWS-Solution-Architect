a57ae3e5457dde25887543040d2a6b221ddf0158
Tochukwu Nwachukwu <truetochukz@gmail.com>
2025-12-05 15:02:11 +0200

    Moved General Solution to Examples

--- general-solutions/sns-fanout/deploy.sh
+++
@@ -1,5 +0,0 @@
-
-version=v0.0.1
-cd lambda-code 
-zip -r lambda-code.zip . -x "lambda-code.zip"
-aws s3 cp lambda-code.zip s3://chucks-workspace-storage/$version/lambda-code.zip
--- general-solutions/sqs-to-lambda/lambda.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async (event) => {
-  const records = JSON.stringify(event.Records);
-  const time = new Date().toTimeString();
-  console.log("Received SQS messages:", { records, time });
-  return {
-    statusCode: 200,
-    body: "Processed successfully",
-  };
-};
